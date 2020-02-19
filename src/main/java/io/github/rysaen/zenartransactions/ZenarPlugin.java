7cad0fee9ba0c88fe0058960b27acc5b6fa5c553
Rysaen <francesco.costantino@outlook.com>
2020-02-19 17:57:33 +0100

    Version update

--- src/main/java/io/github/rysaen/zenartransactions/ZenarPlugin.java
+++ src/main/java/io/github/rysaen/zenartransactions/ZenarPlugin.java
@@ -51,7 +51,7 @@ public class ZenarPlugin {
 	public final static String ID = "zenartransactions";
 	public final static String NAME = "Zenar Transactions";
 	public final static String DESCRIPTION = "Zenar Transactions Plugin";
-	public final static String VERSION = "20.1.7";
+	public final static String VERSION = "20.2.19";
 	public final static String SPONGE_API_VERSION = "7.1.0";
 
 	@Inject PluginContainer plugin;
